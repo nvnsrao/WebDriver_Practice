f9075caf5924a9f8ab5dfbcd1aae4fca581ee3d9
Hello <Hello@192.168.0.7>
2018-02-04 11:47:11 +0530

    Maven Project to Git

---
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,34 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	WebDriver driver;
+	
+  @Test
+  public void f() {
+	  
+	  driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 
+  }
+  
+  @BeforeTest
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "WebDriver/chromedriver.exe");
+		driver= new ChromeDriver();
+		//driver.get("http://demo.guru99.com/v4/index.php");
+	}
+  
+  @AfterClass
+  public void close() {
+ 	 driver.close();
+  }
+  
+  
+}
